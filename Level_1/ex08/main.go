addca2ece61225f95fc0ac7d5af7514b6e86cffd
Alive Wells <alivewel@pr-o2.kzn.21-school.ru>
2023-10-25 16:43:13 +0300

    add ex08

--- Level_1/ex08/main.go
+++ Level_1/ex08/main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 )
 
-func getBit(num int64, index int64) (int, error) {
+func getBit(num int64, index int) (int, error) {
 	if index < 0 || index > 63 {
 		return 0, errors.New("Invalid index. Index must be in the range [0, 63].")
 	}
@@ -18,8 +18,23 @@ func getBit(num int64, index int64) (int, error) {
 	// return (num & mask) != 0, nil
 }
 
+func setBit(num *int64, index int, bit int) error {
+	if bit != 1 && bit != 0 {
+		return errors.New("Invalid bit value. Bit must be 0 or 1.")
+	}
+	if index < 0 || index > 64 {
+		return errors.New("Invalid index. Index must be in the range [0, 63].")
+	}
+	if bit == 1 {
+		*num = *num | 1<<index
+	} else {
+		*num = *num &^ (1 << index)
+	}
+	return nil
+}
+
 func printBits(num int64) {
-	for i := int64(63); i >= 0; i-- {
+	for i := 63; i >= 0; i-- {
 		tmp, _ := getBit(num, i)
 		fmt.Printf("%d", tmp)
 	}
@@ -27,10 +42,27 @@ func printBits(num int64) {
 }
 
 func main() {
-	var num int64 = -2
+	// var num int64 = 0
+	var num int64 = 9223372036854775807
 	printBits(num)
-	var num1 int64 = -9223372036854775808 
+
+	// for i := 0; i <= 63; i++ {
+	// 	err := setBit(&num, i, 0)
+	// 	if err != nil {
+	// 		fmt.Println("Ошибка:", err)
+	// 	}
+	// 	printBits(num)
+	// }
+	var num1 int64 = 9223372036854775807
+	err := setBit(&num1, 64, 0)
+	if err != nil {
+		fmt.Println("Ошибка:", err)
+	}
 	printBits(num1)
-	var num2 int64 = 9223372036854775807
-	printBits(num2)
+	print(num1)
+
+	// var num1 int64 = -9223372036854775808
+	// printBits(num1)
+	// var num2 int64 = 9223372036854775807
+	// printBits(num2)
 }
