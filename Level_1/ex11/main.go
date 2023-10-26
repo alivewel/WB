c33e394c0fbc5a2ef6e63f9a1a4724773c68c52b
Alive Wells <alivewel@pr-o2.kzn.21-school.ru>
2023-10-26 16:50:20 +0300

    add ex09 ex11

--- Level_1/ex11/main.go
+++ Level_1/ex11/main.go
@@ -2,6 +2,62 @@ package main
 
 import "fmt"
 
+func intersec(set1, set2 map[int]struct{}) map[int]struct{} {
+	result := make(map[int]struct{})
+
+	for elem := range set1 {
+		// проверка, существует ли element во втором множестве set2
+		if _, exists := set2[elem]; exists {
+			result[elem] = struct{}{}
+		}
+	}
+
+	return result
+}
+
+// менее оптимизированный способ
+func intersec1(set1, set2 map[int]struct{}) map[int]struct{} {
+	result := make(map[int]struct{})
+
+	for elem1 := range set1 {
+		for elem2 := range set2 {
+			if elem1 == elem2 {
+				result[elem1] = struct{}{}
+			}
+		}
+	}
+
+	return result
+}
+
+func intersec2(set1, set2 map[int]struct{}) map[int]struct{} {
+	result := make(map[int]struct{})
+
+	for elem := range set1 {
+		// проверка, существует ли element во втором множестве set2
+		_, ok := set2[elem]
+		// If the key exists
+		if ok {
+			result[elem] = struct{}{}
+		}
+	}
+
+	return result
+}
+
 func main() {
-	fmt.Println("Hello!")
+	set1 := make(map[int]struct{})
+	set2 := make(map[int]struct{})
+
+	set1[1] = struct{}{}
+	set1[2] = struct{}{}
+	set1[3] = struct{}{}
+
+	set2[2] = struct{}{}
+	set2[3] = struct{}{}
+	set2[4] = struct{}{}
+
+	intersec := intersec2(set1, set2)
+
+	fmt.Println(intersec)
 }
