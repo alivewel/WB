8dd3c87f9bfb0ff31bfc4780d091b3e07d88aa89
Alive Wells <alivewel@pr-o2.kzn.21-school.ru>
2023-10-29 00:57:47 +0300

    add ex 26

--- Level_1/ex26/main.go
+++ Level_1/ex26/main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+func checkString(str string) bool {
+	checkMap := make(map[rune]struct{})
+	for _, elem := range str {
+		_, ok := checkMap[elem]
+		if ok {
+			return false
+		}
+		checkMap[elem] = struct{}{}
+	}
+	return true
+}
+
 func main() {
-	fmt.Println("Hello!")
+	str := "abcda"
+	result := checkString(str)
+	fmt.Println(result)
 }
