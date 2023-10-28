57f859dcd63c3655b961f8a173ea565f99ca5a96
Alive Wells <alivewel@pr-o2.kzn.21-school.ru>
2023-10-28 19:46:00 +0300

    add ex23

--- Level_1/ex23/main.go
+++ Level_1/ex23/main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello!")
+	a := []int{1, 2, 3, 4, 5, 6, 7}
+
+	// a = append(a[0:2], a[3:]...) // элемент в середине
+
+	b := make([]int, 0, len(a)-1)
+	// a = append(b, a[1:]...) // первый элемент
+	a = append(b, a[0:(len(a)-1)]...) // последний элемент
+
+	fmt.Println(a) // [1 2 4 5 6 7]
 }
