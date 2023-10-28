1216830091d9e066012cd1f6c2e5920ac1eafdc0
Alive Wells <alivewel@pr-o2.kzn.21-school.ru>
2023-10-28 16:48:48 +0300

    add ex18

--- Level_1/ex18/main.go
+++ Level_1/ex18/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+type counter struct {
+	count int
+	mu    sync.Mutex
+}
+
+func (c *counter) Increment(wg *sync.WaitGroup) {
+	defer wg.Done()
+	c.mu.Lock()
+	c.count++
+	c.mu.Unlock()
+}
 
 func main() {
-	fmt.Println("Hello!")
+	count := new(counter)
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go count.Increment(&wg)
+	}
+	wg.Wait()
+	fmt.Println(count.count)
 }
+
+// Реализовать структуру-счетчик, которая будет инкрементироваться в конкурентной среде.
+// По завершению программа должна выводить итоговое значение счетчика.
