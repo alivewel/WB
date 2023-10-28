06307e3aed5c848123ae7ed760206b9e0630e7c8
Alive Wells <alivewel@pr-o2.kzn.21-school.ru>
2023-10-29 01:19:53 +0300

    add ex19

--- Level_1/ex19/main.go
+++ Level_1/ex19/main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+func reverseString(str string) string {
+	runes := []rune(str)
+	reverseStr := make([]rune, len(runes))
+	for i := len(runes) - 1; i >= 0; i-- {
+		reverseStr = append(reverseStr, runes[i])
+	}
+	return string(reverseStr)
+}
+
 func main() {
-	fmt.Println("Hello!")
+	str := "Привет, 🌍! 你好, 世界!"
+	fmt.Println(str)
+	result := reverseString(str)
+	fmt.Println(result)
 }
