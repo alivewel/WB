b2c99349e3e723e1298da2d0b9f0a3f564b4b7e9
Alive Wells <alivewel@pr-o2.kzn.21-school.ru>
2023-10-28 16:05:09 +0300

    add ex16 ex17

--- Level_1/ex16/main.go
+++ Level_1/ex16/main.go
@@ -2,6 +2,36 @@ package main
 
 import "fmt"
 
+func partition(arr []int, low, high int) ([]int, int) {
+	pivot := arr[high]
+	i := low
+	for j := low; j < high; j++ {
+		if arr[j] < pivot {
+			arr[i], arr[j] = arr[j], arr[i]
+			i++
+		}
+	}
+	arr[i], arr[high] = arr[high], arr[i]
+	return arr, i
+}
+
+func quickSort(arr []int, low, high int) []int {
+	if low < high {
+		var p int
+		arr, p = partition(arr, low, high)
+		arr = quickSort(arr, low, p-1)
+		arr = quickSort(arr, p+1, high)
+	}
+	return arr
+}
+
+func quickSortStart(arr []int) []int {
+	return quickSort(arr, 0, len(arr)-1)
+}
+
 func main() {
-	fmt.Println("Hello!")
+	arr := []int{1, 12, 5, 32, 3, 9, 23, 7}
+	fmt.Println(arr)
+	quickSortStart(arr)
+	fmt.Println(arr)
 }
--- Level_1/ex17/main.go
+++ Level_1/ex17/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func binarySearch(arr []int, target int) int {
+	left, right := 0, len(arr)-1
+
+	for left <= right {
+		mid := left + (right-left)/2
+
+		if arr[mid] == target {
+			return mid
+		} else if arr[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+
+	return -1 // Если элемент не найден
+}
 
 func main() {
-	fmt.Println("Hello!")
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	target := 5
+
+	index := binarySearch(arr, target)
+	if index != -1 {
+		fmt.Printf("Элемент %d найден в позиции %d\n", target, index)
+	} else {
+		fmt.Printf("Элемент %d не найден\n", target)
+	}
 }
