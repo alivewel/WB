0d8bdbf95d0337a66b455ed4ed961df1afe71b0f
Alive Wells <alivewel@pr-o2.kzn.21-school.ru>
2023-10-30 15:19:40 +0300

    add ex20

--- Level_1/ex20/main.go
+++ Level_1/ex20/main.go
@@ -5,16 +5,18 @@ import (
 	"strings"
 )
 
-func main() {
-	s := "GeeksforGeeks is a computer science portal !"
-	v := strings.Fields(s)
-	for i, j := range v {
-		fmt.Printf("%d %s\n", i, j)
+func reverseString(inputStr string) string {
+	strArr := strings.Fields(inputStr)
+	lenStr := len(strArr)
+	revArr := make([]string, lenStr)
+	for i, j := 0, lenStr-1; i < lenStr; i, j = i+1, j-1 {
+		revArr[i] = strArr[j]
 	}
-	newArr := make([]string, len(v))
-	for i, j := 0, len(v)-1; i < len(v); i, j = i+1, j-1 {
-		newArr[i] = v[j]
-	}
+	return strings.Join(revArr, " ")
+}
 
-	fmt.Println(newArr)
+func main() {
+	str := "snow dog sun"
+	revStr := reverseString(str)
+	fmt.Println(revStr)
 }
