b76c35a428ac8360351dd25029979fe0fcf11608
Alive Wells <alivewel@pr-o2.kzn.21-school.ru>
2023-10-30 17:25:47 +0300

    add ex22

--- Level_1/ex22/go.mod
+++ Level_1/ex22/go.mod
@@ -1,3 +1,5 @@
 module ex22
 
 go 1.19
+
+require github.com/shopspring/decimal v1.3.1 // indirect
--- Level_1/ex22/main.go
+++ Level_1/ex22/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+
+	"github.com/shopspring/decimal"
+)
+
+// Разработать программу, которая перемножает, делит, складывает, вычитает
+// две числовых переменных a,b, значение которых > 2^20.
 
 func main() {
-	fmt.Println("Hello!")
+	int1 := int64(math.Pow(2, 30))
+	int2 := int64(math.Pow(2, 20))
+
+	num1 := decimal.NewFromInt(int1)
+	num2 := decimal.NewFromInt(int2)
+
+	sum := num1.Add(num2)
+	subtraction := num1.Sub(num2)
+	product := num1.Mul(num2)
+	division, _ := num1.Div(num2).Round(2).Float64()
+
+	fmt.Println("Сумма:", sum)
+	fmt.Println("Разность:", subtraction)
+	fmt.Println("Произведение:", product)
+	fmt.Println("Деление:", division)
 }
