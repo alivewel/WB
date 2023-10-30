5f2f45dd3082d667b120bf3741bc106e036c9008
Alive Wells <alivewel@pr-o2.kzn.21-school.ru>
2023-10-30 15:03:03 +0300

    add ex20

--- Level_1/ex20/main.go
+++ Level_1/ex20/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello!")
+	s := "GeeksforGeeks is a computer science portal !"
+	v := strings.Fields(s)
+	for i, j := range v {
+		fmt.Printf("%d %s\n", i, j)
+	}
+	newArr := make([]string, len(v))
+	for i, j := 0, len(v)-1; i < len(v); i, j = i+1, j-1 {
+		newArr[i] = v[j]
+	}
+
+	fmt.Println(newArr)
 }
