532665b89ee8df2aab6a08bec0130c7a26558718
Alive Wells <alivewel@va-h1.kzn.21-school.ru>
2023-11-28 15:32:22 +0300

    edit ex05

--- Level_2/ex05/main.go
+++ Level_2/ex05/main.go
@@ -7,12 +7,40 @@ import (
 	"os"
 )
 
+type Flags struct {
+	flagA     int
+	flagB     int
+	flagC     int
+	flagCount bool
+	count     int
+	flagI     bool
+	flagV     bool
+	flagF     bool
+	flagN     bool
+}
+
+var flags Flags
+
+func init() {
+	flag.IntVar(&flags.flagA, "A", 0, "Print N lines after each match")
+	flag.IntVar(&flags.flagB, "B", 0, "Print N lines before each match")
+	flag.IntVar(&flags.flagC, "C", 0, "Print N lines of output context")
+	flag.BoolVar(&flags.flagCount, "count", false, "Print only the number of matches")
+	flag.IntVar(&flags.count, "с", 0, "Print only the number of matches")
+	flag.BoolVar(&flags.flagI, "i", false, "Case-insensitive match")
+	flag.BoolVar(&flags.flagV, "v", false, "Select non-matching lines")
+	flag.BoolVar(&flags.flagF, "F", false, "Fixed string (disable regular expressions)")
+	flag.BoolVar(&flags.flagN, "n", false, "Show line numbers")
+	flag.Parse()
+}
+
 func main() {
 	// Парсинг флагов
-	afterFlag := flag.Int("A", 0, "Print N lines after each match")
-	beforeFlag := flag.Int("B", 0, "Print N lines before each match")
-	contextFlag := flag.Int("C", 0, "Print N lines of output context")
-	flag.Parse()
+	fmt.Println("Flag A:", flags.flagA)
+	fmt.Println("Flag B:", flags.flagB)
+	fmt.Println("Flag C:", flags.flagC)
+	fmt.Println("Flag Count:", flags.flagCount)
+	fmt.Println("Flag N:", flags.flagN)
 
 	// Получение остальных аргументов (паттерн и файлы)
 	args := flag.Args()
@@ -21,23 +49,23 @@ func main() {
 		os.Exit(1)
 	}
 
-	pattern := args[0]
-	files := args[1:]
+	// pattern := args[0]
+	// files := args[1:]
 
-	fmt.Println("afterFlag", *afterFlag)
-	fmt.Println("beforeFlag", *beforeFlag)
-	fmt.Println("contextFlag", *contextFlag)
+	// fmt.Println("afterFlag", *afterFlag)
+	// fmt.Println("beforeFlag", *beforeFlag)
+	// fmt.Println("contextFlag", *contextFlag)
 
 	// fmt.Println("files", files)
 	// fmt.Println("pattern", pattern)
 
 	// Применение фильтрации к каждому файлу
-	for _, file := range files {
-		err := grepFile(pattern, file, *afterFlag, *beforeFlag, *contextFlag)
-		if err != nil {
-			fmt.Println("Error:", err)
-		}
-	}
+	// for _, file := range files {
+	// 	err := grepFile(pattern, file, *afterFlag, *beforeFlag, *contextFlag)
+	// 	if err != nil {
+	// 		fmt.Println("Error:", err)
+	// 	}
+	// }
 }
 
 func grepFile(pattern, filename string, after, before, context int) error {
@@ -48,50 +76,20 @@ func grepFile(pattern, filename string, after, before, context int) error {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	matchingLines := make([]string, 0)
-	contextLines := make([]string, 0)
 
 	for scanner.Scan() {
 		line := scanner.Text()
 
 		// Проверка на совпадение паттерна
 		if containsPattern(line, pattern) {
-			// Выводим строки до совпадения (если есть)
-			for _, beforeLine := range contextLines {
-				fmt.Println(beforeLine)
-			}
-			contextLines = contextLines[:0]
-
-			// Выводим строки совпадения
 			fmt.Println(line)
-
-			// Выводим строки после совпадения (если есть)
-			for i := 1; i <= after; i++ {
-				if scanner.Scan() {
-					fmt.Println(scanner.Text())
-				}
-			}
-			matchingLines = matchingLines[:0]
-		} else {
-			// Если не совпало, добавляем строку в контекст
-			contextLines = append(contextLines, line)
-
-			// Поддержка флага -C (контекст)
-			if len(contextLines) > context {
-				contextLines = contextLines[1:]
-			}
 		}
-
-		// Поддержка флага -B (перед совпадением)
-		if len(matchingLines) > before {
-			matchingLines = matchingLines[1:]
-		}
-		matchingLines = append(matchingLines, line)
 	}
 
 	return nil
 }
 
 func containsPattern(line, pattern string) bool {
+
 	return true // Ваша реализация проверки наличия паттерна в строке
 }
