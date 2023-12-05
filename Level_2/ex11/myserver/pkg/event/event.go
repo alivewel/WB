a2b8f5261d51925ae8e997505d74b5ee3a172bfb
Alive Wells <alivewel@va-h1.kzn.21-school.ru>
2023-12-06 00:16:22 +0300

    add ex11

---
+++ Level_2/ex11/myserver/pkg/event/event.go
@@ -0,0 +1,33 @@
+package event
+
+import (
+	"errors"
+	"fmt"
+	// "sync"
+
+	"time"
+)
+
+// временное дублирование
+// потом вынести в отдельный файл
+type Event struct {
+	Summary string    `json:"summary"`
+	Date    time.Time `json:"date"`
+}
+
+// функция которая создает новую структуру Event (конструктор)
+func NewEvent(summary, date string) (Event, error) {
+	parsedTime, err := time.Parse("2006-01-02", date)
+	if err != nil {
+		return Event{}, fmt.Errorf("ошибка при разборе даты: %v", err)
+	}
+
+	if summary == "" || parsedTime == (time.Time{}) {
+		return Event{}, errors.New("некорректные данные для создания события")
+	}
+
+	return Event{
+		Summary: summary,
+		Date:    parsedTime,
+	}, nil
+}
